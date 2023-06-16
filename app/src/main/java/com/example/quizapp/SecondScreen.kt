cd321b760a8e4bd39680f7c2474a99fddeadbf9e
snchild <sierrachild@yahoo.com>
2023-06-16 11:14:58 -0600

    cleaning up code

--- app/src/main/java/com/example/quizapp/SecondScreen.kt
+++ app/src/main/java/com/example/quizapp/SecondScreen.kt
@@ -5,15 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.quizapp.ui.theme.QuizAppTheme
 import kotlin.random.Random
 
 class SecondScreen : ComponentActivity() {
